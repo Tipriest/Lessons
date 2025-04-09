4a30c82b2a7adbc6b13ff5f3f4cc56a1a39040c9
Tipriest <a1503741059@163.com>
2025-04-09 22:20:13 +0800

    update .gitmodules

--- 编译原理/CMake基础/src/main.cpp
+++ 编译原理/CMake基础/src/main.cpp
@@ -1,14 +1,16 @@
-#include <iostream>
-#include <vector>
+#include "../include/Solution.h"
 #include <algorithm>
 #include <cmath>
-#include "../include/Solution.h"
+#include <iostream>
+#include <vector>
 
 using namespace std;
-int main(int argc, char ** argv){
-    Solution solution;
-    vector<int> dist = {1, 1, 100000};
-    double hour = 2.01;
-    cout<< solution.minSpeedOnTime(dist, hour)<<endl;
-    return 1;
+int main(int argc, char **argv) {
+  Solution solution;
+  vector<int> dist = {1, 1, 100000};
+  double hour = 2.01;
+  cout << solution.minSpeedOnTime(dist, hour) << endl;
+  return 1;
+
+  
 }
